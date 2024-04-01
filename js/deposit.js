94cc5eba5cb7fc1fb112df6c54aa697b1a9c2761
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-04-01 23:34:56 +0600

    Get User Deposit And Set Deposit Total Value

--- js/deposit.js
+++ js/deposit.js
@@ -1,5 +1,8 @@
 document.getElementById("btn-deposit").addEventListener("click", function () {
   const depositField = document.getElementById("deposit-field");
   const depositeAmount = depositField.value;
-  console.log(depositeAmount);
+
+  const depositeTotalElement = document.getElementById("deposit-total");
+  const depositTotal = depositeTotalElement.innerText;
+  depositeTotalElement.innerText = depositeAmount;
 });
